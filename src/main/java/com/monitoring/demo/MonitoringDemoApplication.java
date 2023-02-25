8cf19532ad0301c66aa8bde85b62faf28d925586
Ram M <ramm@Rams-MacBook-Pro.local>
2023-02-25 12:27:15 +0800

    initial changes

---
+++ src/main/java/com/monitoring/demo/MonitoringDemoApplication.java
@@ -0,0 +1,18 @@
+package com.monitoring.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+public class MonitoringDemoApplication {
+
+	@GetMapping
+	public String message(){
+		return "Welcome Java";
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(MonitoringDemoApplication.class, args);
+	}
+
+}
